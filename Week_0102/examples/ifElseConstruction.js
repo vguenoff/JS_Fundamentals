8503f64248701c69014b1ecbeb1329610a0f3734
Vassil Guenoff <vassilguenoff@Vassils-MacBook-Air.local>
2016-05-02 21:39:12 +0300

    examples added

---
+++ Week_0102/examples/ifElseConstruction.js
@@ -0,0 +1,7 @@
+var number = 27;
+
+if (number % 2) {
+    console.log('Odd number.');
+} else {
+    console.log('Even number.');
+}
---
+++ Week_0102/examples/simpleIfConstruction.js
@@ -0,0 +1,9 @@
+var sideA = 5,
+    sideB = 7,
+    biggerSide;
+
+if (sideB > sideA) {
+    biggerSide = sideB;
+}
+
+console.log(biggerSide);
