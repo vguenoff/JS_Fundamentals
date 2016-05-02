d6b6b840e2ddaa6095ade910d4cac4413df99db5
Vassil Guenoff <vassilguenoff@Vassils-MacBook-Air.local>
2016-05-02 22:24:52 +0300

    homework task 01, 02, 03 ready

---
+++ Week_0102/homework/01/script.js
@@ -0,0 +1,14 @@
+var number = 6;
+
+switch (number) {
+    case 1: console.log('One'); break;
+    case 2: console.log('Two'); break;
+    case 3: console.log('Three'); break;
+    case 4: console.log('Four'); break;
+    case 5: console.log('Five'); break;
+    case 6: console.log('Six'); break;
+    case 7: console.log('Seven'); break;
+    case 8: console.log('Eight'); break;
+    case 9: console.log('Nine'); break;
+    case 10: console.log('Ten'); break;
+}
---
+++ Week_0102/homework/03/script.js
@@ -0,0 +1,1 @@
+console.log([13, 5, 55].sort(function(a, b) { return a - b; }));
